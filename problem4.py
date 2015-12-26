cbc4dba3add267ef01c265391439c05589b2d08f
azreal45 <jgconelly@gmail.com>
2015-12-26 09:08:39 -0500

    Changed p4 to use range instead

--- problem4.py
+++ problem4.py
@@ -1,10 +1,10 @@
 #Project Euler Problem 4
 largest_pal = 0
-for a in xrange(999, 100, -1): #xrange seemed like the cleanest way to do this. 
-    for b in xrange(a, 100, -1):#working from the largest 3 digit and moving backwards. 
-        pal = a * b
+for x in range(999, 100, -1): #range seemed like the cleanest way to do this. 
+    for y in range(x, 100, -1):#working from the largest 3 digit and moving backwards. 
+        pal = x * y
         if pal > largest_pal:
-            s = str(a * b) #flip to a string so we can check if this is really a palindrome
+            s = str(x * y) #flip to a string so we can check if this is really a palindrome
             if s == s[::-1]:
-                 largest_pal = a * b
+                 largest_pal = x * y
 print largest_pal
